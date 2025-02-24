f59d2621a4eed7458e6b27a0991d155f05a40312
NiuhiPlays <nielsiansmeets@gmail.com>
2025-02-24 23:23:51 +0100

    Initial commit

---
+++ elytra-mod-template-1.21.4/src/client/java/niuhi/elytra/ElytraModClient.java
@@ -0,0 +1,10 @@
+package niuhi.elytra;
+
+import net.fabricmc.api.ClientModInitializer;
+
+public class ElytraModClient implements ClientModInitializer {
+	@Override
+	public void onInitializeClient() {
+		// This entrypoint is suitable for setting up client-specific logic, such as rendering.
+	}
+}
